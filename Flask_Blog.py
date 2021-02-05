acf31068d5c4719fff9d67e25b757230df9b1d94
Houman <houman@hotmail.co.uk>
2021-02-05 02:13:13 +0000

    Added HTML and more functionality

--- Flask_Blog.py
+++ Flask_Blog.py
@@ -1,17 +1,29 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {"author": "Houman Houshidar",
+     "title": "arm wrestling pro question",
+     "content": "how do I become a pro arm wrestler?",
+     "date_posted": "03/02/2021"},
+    {"author": "Cyrus Houshidar",
+     "title": "response - arm wrestling pro question",
+     "content": "hard work and skill",
+     "date_posted": "05/02/2021"}
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    name = request.args.get("name", "Page")
-    return f'<h1>Home, {escape(name)}!</h1>'
+
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    name = request.args.get("name", "Page")
-    return f'<h1>About, {escape(name)}!</h1>'
+
+    return render_template('about.html', title="about")
 
 
 if __name__ == '__main__':

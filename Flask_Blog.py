31a2164cbfc59825a55c882e2eb4cbc9a08f0190
Houman <houman@hotmail.co.uk>
2021-02-09 23:27:57 +0000

    Updated with register.html

--- Flask_Blog.py
+++ Flask_Blog.py
@@ -1,6 +1,9 @@
 from flask import Flask, escape, request, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "756fcb446d673b4bd20d82da45cf17ca"
+
 
 posts = [
 
@@ -25,6 +28,16 @@ def home():
 def about():
 
     return render_template('about.html', title="about")
+
+@app.route('/register')
+def registration():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("Login.html", title="Login", form=form)
 
 
 if __name__ == '__main__':
